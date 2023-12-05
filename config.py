b2c6a018d2aa316c559db64f8060012f4b8f1ace
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-12-05 14:17:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Noobxcoders/AliciaMusic",
+    "https://github.com/Noobxcoders/Alicia-Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
