2b27cdfb86fd5de30dd66a5b8e223fc5d2bea51a
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-12-09 08:16:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,14 +39,14 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/Our_Groupps",
+    "https://t.me/hopeless_boy",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Our_Groupps")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/hopeless_boy")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AliciaNetwork")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
